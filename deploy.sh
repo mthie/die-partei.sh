fa2e7c04c75718ea06b4fd9085e1bd835983c216
Martin Thielecke <github@mthie.com>
2021-11-08 22:10:10 +0100

    remove old deployment

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,4 @@
 
 set -xe
 
-if [ -d "public" ]; then
-  rm -r public
-fi
-
-/usr/local/bin/hugo-die-partei-cms
-firebase --project die-partei-sh deploy
-
-if [ -d "public" ]; then
-  rm -r public
-fi
+# deployment is done by github
